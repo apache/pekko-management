8ee30d275bd245e646b4fb4b6082e46b53e0cb86
PJ Fanning <pjfanning@users.noreply.github.com>
2023-08-05 09:58:24 +0100

    add MIT license for some test scripts

--- project/MetaInfLicenseNoticeCopy.scala
+++ project/MetaInfLicenseNoticeCopy.scala
@@ -18,18 +18,21 @@
 import sbt.Keys._
 import sbt._
 import org.mdedetrich.apache.sonatype.ApacheSonatypePlugin
-import ApacheSonatypePlugin.autoImport.apacheSonatypeDisclaimerFile
+import ApacheSonatypePlugin.autoImport.{ apacheSonatypeDisclaimerFile, apacheSonatypeLicenseFile }
 
 /**
  * Copies LICENSE and NOTICE files into jar META-INF dir
  */
 object MetaInfLicenseNoticeCopy extends AutoPlugin {
 
+  private lazy val baseDir = LocalRootProject / baseDirectory
+
   override def trigger = allRequirements
 
   override def requires = ApacheSonatypePlugin
 
   override lazy val projectSettings = Seq(
-    apacheSonatypeDisclaimerFile := Some((LocalRootProject / baseDirectory).value / "DISCLAIMER"))
+    apacheSonatypeLicenseFile := baseDir.value / "legal" / "StandardLicense.txt",
+    apacheSonatypeDisclaimerFile := Some(baseDir.value / "DISCLAIMER"))
 
 }
