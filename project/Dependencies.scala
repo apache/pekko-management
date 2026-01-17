64ad296e5077b7f4d049bcbb7190cb794c3e9b5f
Scala Steward <scala_steward@virtuslab.com>
2026-01-17 16:26:53 +0000

    Update logback-classic to 1.5.25

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val jacksonVersion = "2.20.1"
 
   val log4j2Version = "2.25.3"
-  val logbackVersion = "1.5.24"
+  val logbackVersion = "1.5.25"
   val slf4jVersion = "2.0.17"
 
   // often called-in transitively with insecure versions of databind / core
