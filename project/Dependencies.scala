872cfe9240d5f762c7df9473da13a7a846ef6e8a
Scala Steward <scala_steward@virtuslab.com>
2026-05-07 12:35:46 +0000

    Update log4j-api, log4j-core, ... to 2.26.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   val guavaVersion = "33.6.0-jre"
   val jacksonVersion = "2.21.3"
 
-  val log4j2Version = "2.25.4"
+  val log4j2Version = "2.26.0"
   val logbackVersion = "1.5.32"
   val slf4jVersion = "2.0.17"
 
