73d8a2c65e27aa7458ee7046de73e906f1fa7e84
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-12 22:40:28 +0200

    Update scala3-library to 3.3.8 (#823)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ import sbt._
 object Dependencies {
   // keep in sync with .github/workflows/unit-tests.yml
   val scala213Version = "2.13.18"
-  val scala3Version = "3.3.7"
+  val scala3Version = "3.3.8"
   val crossScalaVersions = Seq(scala213Version, scala3Version)
 
   val pekkoVersion = PekkoCoreDependency.version
